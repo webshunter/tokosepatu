1f04a923a1c6997ca5df04508f1c6332e2c844b3
webshunter <webshunternet@gmail.com>
2023-10-11 20:38:27 +0700

    gugus update

--- app/component/header.js
+++ app/component/header.js
@@ -12,16 +12,10 @@ export const Header = function(){
     })
 
     return (<>
-        <button className="flex items font-bold text-ble-800 center  top-1 right-1 z-[9999] rounded-[20px] drop-shadow-lg fixed ">
-            <Image
-                priority
-                className="h-[40px] cursor-pointer w-[auto]"
-                src={btn}
-                alt="Follow us on Twitter"
-            />
-        </button>
-        <nav className="fixed top-0 z-[999] h-[50px] w-[100vw] bg-blue-600 text-white px-10 py-2">
-            <Link href="/">RumahJo</Link>
+        <nav className="fixed top-0 z-[999] h-[85px] w-[100vw] bg-indigo-950 text-white px-10 py-2">
+            <Link href="/">
+                <img src="rumahjocom.png" className="w-[200px] pt-3"></img>
+            </Link>
         </nav>
         <nav className="px-10 py-2 shadow-md text-gray-600">
             <button className="pr-5">SEMUA KATEGORI</button>
