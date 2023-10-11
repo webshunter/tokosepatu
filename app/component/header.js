c8be39ceab3f9d62b0f69745dbb99d43ee0d4322
webshunter <webshunternet@gmail.com>
2023-10-11 20:32:54 +0700

    update

--- app/component/header.js
+++ app/component/header.js
@@ -1,6 +1,10 @@
 "use client"
 import Link from "next/link"
 import { useEffect } from "react"
+import buttonJual from "./buttonJual"
+import btn from '../component/button2.svg';
+import Image from "next/image"
+
 export const Header = function(){
 
     useEffect(()=>{
@@ -8,6 +12,14 @@ export const Header = function(){
     })
 
     return (<>
+        <button className="flex items font-bold text-ble-800 center  top-1 right-1 z-[9999] rounded-[20px] drop-shadow-lg fixed ">
+            <Image
+                priority
+                className="h-[40px] cursor-pointer w-[auto]"
+                src={btn}
+                alt="Follow us on Twitter"
+            />
+        </button>
         <nav className="fixed top-0 z-[999] h-[50px] w-[100vw] bg-blue-600 text-white px-10 py-2">
             <Link href="/">RumahJo</Link>
         </nav>
