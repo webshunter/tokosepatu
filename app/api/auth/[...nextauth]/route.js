395bf88c1215cc88b5b7aca589169a2efc9a39b7
webshunternet <webshunternet@gmail.com>
2023-10-24 07:53:39 +0700

    update login

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,8 +1,33 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
+import  CredentialsProvider  from "next-auth/providers/credentials";
 
 const handler = NextAuth({
     providers: [
+
+        CredentialsProvider({
+            name: "",
+            credentials: {
+                username: {
+                    label: "Email:",
+                    type: "text",
+                    placeholder: ""
+                }
+                , password: {
+                    label: "Password:",
+                    type: "password",
+                    placeholder: ""
+                }
+            },
+            async authorize(credentials) {
+                const user = { id: "42", name: "Demo", username: "demo@gmail.com", password: "demo" }
+                if (credentials?.username === user.username && credentials?.password === user.password) {
+                    return user;
+                } else {
+                    return null;
+                }
+            }
+        }),
         GoogleProvider({ 
             clientId: process.env.GOOGLE_CLIENT_ID ?? ""
             , clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "", 
