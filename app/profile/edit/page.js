58b0cbe9479a37a57ce49c3bf0016140a1b0c306
Yogo Kurniawan <awan_borneo@null.net>
2023-10-29 08:36:59 +0700

    Profile

---
+++ app/profile/edit/page.js
@@ -0,0 +1,157 @@
+"use client"
+import { useEffect, useState } from 'react';
+
+export default function EditProfile() {
+    const [popupVisible, setPopUpVisible] = useState(false);
+    const openPopUp = () => {
+        setPopUpVisible(true);
+    };
+    const closePopUp = () => {
+        setPopUpVisible(false);
+    };
+    const [fullname, setName] = useState("");
+    return (<>
+        <div className="grid grid-cols-6 gap-4">
+            <div className="col-start-auto md:col-start-2 col-span-6 md:col-span-4 mt-5">
+                <div className="bg-white shadow-md md:mb-2 px-5 md:p-5 rounded-xm">
+                    <div className="hidden md:block pb-[16px]">
+                        <h3 className="text-[20px] font-bold text-gray-800">Edit Profil</h3>
+                    </div>
+                    <div className="pt-[16px] border-b border-gray-400">
+                        <div className="text-[16px] leading-[24px] font-bold py-[8px] text-gray-800 border-0 md:border-t border-gray-400">Informasi Dasar</div>
+                        <div className="pb-[16px]">
+                            <div className="flex items-center md:flex-row md:justify-start box-border flex-wrap">
+                                <div className="basis-[27.5%] md:basis-[15%]">
+                                    <figure className="relative overflow-hidden w-[96px] md:w-[120px] h-[96px] md:h-[120px] bg-[50%] bg-cover rounded-full m-0 bg-[url('https://static.vecteezy.com/system/resources/previews/014/194/215/original/avatar-icon-human-a-person-s-badge-social-media-profile-symbol-the-symbol-of-a-person-vector.jpg')]">
+                                        <div onClick={openPopUp} className="flex md:hidden absolute bottom-0 left-0 right-0 bg-[rgba(0,47,52,.7)] h-[32px] justify-center items-center cursor-pointer">
+                                            <svg className="w-[24px] h-[24px]" viewBox="0 0 1024 1024" fillRule="evenodd"><path d="M670.72 128l42.667 128h182.613l42.667 42.667v554.667l-42.667 42.667h-768l-42.667-42.667v-554.667l42.667-42.667h182.613l42.667-128h317.44zM609.28 213.333h-194.56l-42.667 128h-201.387v469.333h682.667v-469.333h-201.387l-42.667-128zM512 341.333c117.632 0 213.333 95.701 213.333 213.333s-95.701 213.333-213.333 213.333-213.333-95.701-213.333-213.333 95.701-213.333 213.333-213.333zM512 426.667c-70.613 0-128 57.387-128 128s57.387 128 128 128 128-57.387 128-128-57.387-128-128-128z"></path></svg>
+                                        </div>
+                                    </figure>
+                                </div>
+                                <div className="hidden md:block basis-[22.5%] pr-2">
+                                    <div className="relative">
+                                        <div className="flex flex-row flex-wrap items-start relative">
+                                            <div className="items-center flex w-full">
+                                                <button className="border-2 h-[48px] border-yellow-400 w-full inline-flex justify-center items-center box-border cursor-pointer relative overflow-hidden rounded-md">
+                                                    <span>Unggah</span>
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="hidden md:block basis-[22.5%] pl-2">
+                                    <div className="relative">
+                                        <div className="flex flex-row flex-wrap items-start relative">
+                                            <div className="items-center flex w-full">
+                                                <button className="border-2 h-[48px] border-red-500 bg-red-500 text-white w-full inline-flex justify-center items-center box-border cursor-pointer relative overflow-hidden rounded-md">
+                                                    <span>Hapus</span>
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="basis-[72.5%] md:basis-[60%] pl-[16px] md:pl-0 flex flex-row items-start flex-wrap">
+                                    <div className="flex w-full items-center">
+                                        <div className="grow">
+                                            <input type="text"
+                                            value={fullname}
+                                            onChange={(e) => {
+                                                setName(e.target.value);
+                                            }}
+                                                name="fullname" placeholder="Nama Lengkap"
+                                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                            />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="flex items-center md:flex-row md:justify-start box-border flex-wrap">
+                                <div className="basis-[100%] md:basis-[60%] flex flex-row items-start flex-wrap my-4">
+                                    <div className="flex w-full items-center">
+                                        <div className="grow">
+                                            <textarea rows="4" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Tentang saya (opsional)"></textarea>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="text-[16px] leading-[24px] font-bold py-2 text-gray-800 border-t border-gray-400">Informasi Kontak</div>
+                        <div className="pb-[16px]">
+                            <div className="flex items-center md:flex-row md:justify-start box-border flex-wrap my-4">
+                                <div className="basis-[100%] md:basis-[60%] flex flex-row items-start flex-wrap">
+                                    <div className="flex w-full items-center">
+                                        <div className="grow">
+                                                <input type="text"
+                                                    name="telp" placeholder="+6281234567890"
+                                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                                />
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="basis-[100%] md:basis-[40%] flex flex-row items-start flex-wrap">
+                                    <div className='text-[12px] leading-[18px] m-0 md:ml-[8px]'>
+                                        <span>Apakah ini penting?. Hal tersebut penting untuk memungkinkan kami berkomunikasi dengan Anda dengan aman.</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="flex items-center md:flex-row md:justify-start box-border flex-wrap my-4">
+                                <div className="basis-[100%] md:basis-[60%] flex flex-row items-start flex-wrap">
+                                    <div className="flex w-full items-center">
+                                        <div className="grow">
+                                                <input type="text"
+                                                    name="email" placeholder="Email"
+                                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                                />
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="basis-[100%] md:basis-[40%] flex flex-row items-start flex-wrap">
+                                    <div className='text-[12px] leading-[18px] m-0 md:ml-[8px]'>
+                                        <span>Kami tidak akan mengungkapkan email Anda kepada orang lain atau menggunakannya untuk mengirimi Anda spam.</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="flex items-center md:flex-row md:justify-start box-border flex-wrap my-4">
+                                <div className="basis-[100%] md:basis-[60%] flex flex-row items-start flex-wrap">
+                                    <div className="flex w-full items-center">
+                                        <div className="grow">
+                                                <input type="password"
+                                                    name="password" placeholder="Password"
+                                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                                />
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="basis-[100%] md:basis-[40%] flex flex-row items-start flex-wrap">
+                                    <div className='text-[12px] leading-[18px] m-0 md:ml-[8px]'>
+                                        <span>Ini sangat rahasia bagi Anda dan kami akan menjaganya dengan baik pada sitem yang telah kami bangun.</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="pb-[16px]">
+                        <div className="flex items-center md:flex-row md:justify-start box-border flex-wrap">
+                            <div className="basis-[100%] md:basis-[25%] pt-7">
+                                <div className="relative">
+                                    <div className="flex flex-row flex-wrap items-start relative">
+                                        <div className="items-center flex w-full">
+                                            <button type="submit" className="border-2 h-[48px] border-yellow-400 bg-indigo-950 text-white w-full inline-flex justify-center items-center box-border cursor-pointer relative overflow-hidden rounded-md">
+                                                <span>Simpan Perubahan</span>
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    { popupVisible && (
+        <div onClick={closePopUp}>
+            <div className="items-end fixed z-50 flex justify-center bg-[rgba(0,0,0,.8)]" style={{top:"0",left:"0",bottom:"0",right:"0",transform:"translateZ(0)"}}></div>
+        </div>
+    )}
+    </>)
+}
--- app/profile/page.js
+++ app/profile/page.js
@@ -1,33 +1,7 @@
 "use client"
-import { Header } from "@/app/component/header";
-import { useEffect, useState } from "react";
-import { ProdukCard } from '@/app/library/card2';
+import { useState } from 'react';
 
 export default function Profile() {
-    const [dataListing, setDataListing] = useState([]);
-
-    const [card, addCard] = useState([]);
-
-    useEffect(() => {
-
-        if (localStorage.getItem('produkstart') != undefined) {
-            setDataListing(JSON.parse(localStorage.getItem('produkstart')));
-        }
-
-        (async function () {
-            let data = await fetch('https://app.rumahjo.com/data/listing/0/100');
-            data = await data.json();
-            localStorage.setItem('produkstart', JSON.stringify(data));
-            setDataListing(data);
-        })()
-    }, [setDataListing])
-
-    let yh = [];
-    for (let index = 0; index < 12; index++) {
-        yh.push({
-            data: index
-        })
-    }
     return (<>
     <div className='my-5 bg-white'>
         <div className="pt-5 mx-[20px] xl:mx-[60px]">
@@ -40,7 +14,7 @@ export default function Profile() {
                             </div>
                         </div>
                         <div className="flex items-center overflow-hidden whitespace-nowrap text-ellipsis">
-                            <span className="text-[20px] leading-[30px] capitalize w-full font-bold">Rumah Jo</span>
+                            <span className="text-[20px] leading-[30px] capitalize w-full font-bold">Profile Saya</span>
                         </div>
                         <div className="flex flex-col">
                             <div className="my-[20px]">
@@ -78,35 +52,25 @@ export default function Profile() {
                     <div className="hidden md:block w-full my-[26px]">
                         <div className="w-auto">
                             <div className="text-center">
-                                <a className="h-[48px] w-full bg-indigo-950 hover:bg-indigo-750 inline-flex justify-center items-center box-border cursor-pointer relative overflow-hidden rounded-md" style={{color:"rgb(250 204 21)"}}>
+                                <a href={"profile/edit"} className="h-[48px] w-full bg-indigo-950 hover:bg-indigo-750 inline-flex justify-center items-center box-border cursor-pointer relative overflow-hidden rounded-md" style={{color:"rgb(250 204 21)"}}>
                                     <div className="flex mr-[8px]">
-                                        <svg className="w-[22px] h-[22px]" style={{fill:"rgb(250 204 21)"}} viewBox="0 0 1024 1024" fillRule="evenodd"><path d="M768 853.333C720.896 853.333 682.666 815.104 682.666 768C682.666 720.896 720.896 682.667 768 682.667C815.104 682.667 853.333 720.896 853.333 768C853.333 815.104 815.104 853.333 768 853.333M256 597.333C208.896 597.333 170.666 559.104 170.666 512C170.666 464.896 208.896 426.667 256 426.667C303.104 426.667 341.333 464.896 341.333 512C341.333 559.104 303.104 597.333 256 597.333M768 170.667C815.104 170.667 853.333 208.896 853.333 256C853.333 303.104 815.104 341.333 768 341.333C720.896 341.333 682.666 303.104 682.666 256C682.666 208.896 720.896 170.667 768 170.667M768 597.333C715.562 597.333 669.312 621.44 637.994 658.645L424.32 535.253C425.386 527.616 426.666 519.979 426.666 512C426.666 507.221 425.642 502.741 425.258 498.091L643.541 372.053C674.645 405.461 718.72 426.667 768 426.667C862.25 426.667 938.666 350.251 938.666 256C938.666 161.749 862.25 85.3333 768 85.3333C673.749 85.3333 597.333 161.749 597.333 256C597.333 270.208 599.594 283.819 602.837 297.003L396.97 415.915C366.208 370.901 314.581 341.333 256 341.333C161.749 341.333 85.333 417.749 85.333 512C85.333 606.251 161.749 682.667 256 682.667C311.466 682.667 360.234 655.787 391.424 614.827L600.618 735.573C598.57 746.112 597.333 756.907 597.333 768C597.333 862.251 673.749 938.667 768 938.667C862.25 938.667 938.666 862.251 938.666 768C938.666 673.749 862.25 597.333 768 597.333"></path></svg>
+                                        <svg className="w-[22px] h-[22px]" style={{fill:"rgb(250 204 21)"}} viewBox="0 0 1024 1024" fillRule="evenodd"><path d="M822.312 85.333L745.474 162.667L861.792 279.743L938.667 202.409V168.573L855.913 85.333H822.312ZM193.912 178.053L85.334 287.327V829.393L193.912 938.666H732.562L841.14 829.393V554.805L794.107 518.636L747.111 554.805V790.124L693.529 844.05H232.908L179.326 790.124V326.595L232.908 272.67H463.2L493.786 230.083L463.2 178.053H193.912ZM309.3 601.657V718.697H425.618L826.554 315.183L710.199 198.143L309.3 601.657Z"></path></svg>
                                     </div>
-                                    <span>Bagikan Profil</span>
+                                    <span>Edit Profil</span>
                                 </a>
                             </div>
                         </div>
-                        <div className="w-full h-[48px] my-[16px]">
-                            <button className="border-2 h-[48px] border-yellow-400 w-full inline-flex justify-center items-center box-border cursor-pointer relative overflow-hidden rounded-md" style={{color:"rgb(250 204 21)"}}>
-                                <div className="flex mr-[8px]">
-                                    <svg className="w-[22px] h-[22px]" style={{fill:"rgb(250 204 21)"}} viewBox="0 0 1024 1024" fillRule="evenodd"><path d="M127.979 106.667L85.333 149.334V874.686L127.979 917.354L170.647 874.686V618.679H579.681L622.349 661.347H895.448L938.666 618.679V190.187L895.448 149.334H572.343L529.675 106.667H127.979ZM170.647 192.002H494.325L536.992 234.67H853.352V576.012H657.678L615.01 533.344H170.647V192.002Z"></path></svg>
-                                </div>
-                                <span>Laporkan pengguna</span>
-                            </button>
+                        <div className="justify-center flex">
+                            <div className="max-w-full text-[14px] leading-[20px] mt-[26px] text-yellow-400">
+                                <button className="pb-0 capitalize border-b-2 border-yellow-400 h-auto inline-flex justify-center items-center box-border font-bold cursor-pointer relative overflow-hidden">
+                                    <span>Bagikan profil</span>
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
                 <div className="col-span-4 md:col-span-3">
-                    <div className="mt-[10px] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-5">
-                        {dataListing.map((y, i) => {
-                            y.key = i;
-                            return (
-                                <li className="list-none" key={i}>
-                                    <ProdukCard data={y} />
-                                </li>
-                            )
-                        })}
-                    </div>
+                    
                 </div>
             </div>
         </div>
