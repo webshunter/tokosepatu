40011446218f189c981bd3c3e727bdea35487570
webshunternet <webshunternet@gmail.com>
2023-11-22 11:20:00 +0700

    update data

--- app/component/provider.js
+++ app/component/provider.js
@@ -1,10 +1,21 @@
 "use client"
 const { SessionProvider } = require("next-auth/react");
-import React from "react";
+
+import React, { useEffect } from "react";
 
 const Providers = ({children}) => {
+
+    let p = process.env.NODE_ENV
+
+    useEffect(()=>{
+        if(p==='production'){
+            if (window.location.protocol.indexOf('http:') != 'http' && window.location.hostname === 'rumahjo.com'){
+                window.location.href = 'https://rumahjo.com/';
+            }
+        }
+    },[p]);
+
     return (<SessionProvider>{children}</SessionProvider>)
 }
-
 
 export default Providers;
