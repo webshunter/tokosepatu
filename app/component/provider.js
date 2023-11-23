6f77b5327deb9e859705ba7629a91f22b83fddff
webshunternet <webshunternet@gmail.com>
2023-11-23 15:57:26 +0700

    update

--- app/component/provider.js
+++ app/component/provider.js
@@ -5,15 +5,16 @@ import React, { useEffect } from "react";
 
 const Providers = ({children}) => {
 
-
+    const p = process.env.NODE_ENV
     
     useEffect(()=>{
+        console.log(p)
         // if(p==='production'){
         //     if (window.location.protocol.indexOf('http:') != -1 && window.location.hostname === 'rumahjo.com'){
         //         window.location.href = 'https://rumahjo.com/';
         //     }
         // }
-    });
+    },[p]);
 
     return (<SessionProvider>{children}</SessionProvider>)
 }
