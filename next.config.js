e710e14185bd6829cc85cb57aa524ebd2b1cf320
webshunternet <webshunternet@gmail.com>
2023-11-23 11:10:33 +0700

    update

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+            config.resolve.fallback.fs = false
+        }
+
+        return config
+    },
     async headers() {
         return [
             {
