889922a03fda87ef4e46fb85d568107a1d1137af
webshunter <webshunternet@gmail.com>
2024-01-02 14:16:47 +0700

    update

--- app/component/banner/homeBanner.js
+++ app/component/banner/homeBanner.js
@@ -8,7 +8,14 @@ const HomeBanner = ({data}) => {
         <div className='my-6 md:px-10 lg:px-[100px]'>
             <Carousel className="w-full h-[50vw] md:h-[21vw] lg:h-[19vw]">
                 {data.map((d,i) => {
-                    return <li key={i} id={`data-` + d.kode} className={`lg:rounded-b-[25px] `}
+                    return <li key={i} link={d.backlink} onClick={(e)=>{
+                        let w = e.target.getAttribute('link');
+                        if(w){
+                            window.location.href = w;
+                        }else{
+                            window.location.href = '/';
+                        }
+                    }} id={`data-` + d.kode} className={`lg:rounded-b-[25px] `}
                         style={{
                             width: '100%',
                             height: '100%',
