90ec652b6b7860b9a45d243fd1420b146f4ee98e
webshunternet <webshunternet@gmail.com>
2024-02-13 08:10:22 +0700

    update

--- app/component/banner/homeBanner.js
+++ app/component/banner/homeBanner.js
@@ -20,7 +20,7 @@ const HomeBanner = ({data}) => {
                             width: '100%',
                             height: '100%',
                             backgroundRepeat: 'no-repeat',
-                            backgroundSize: 'cover'
+                            backgroundSize: 'contain'
                         }}
                     ></li>
                 })}
