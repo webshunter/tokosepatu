cc231418748aed3dd3b48b0ff3744f2f271bef99
webshunternet <webshunternet@gmail.com>
2024-03-15 12:24:07 +0700

    update

--- app/layout.js
+++ app/layout.js
@@ -8,6 +8,7 @@ import { Footer } from './component/footer';
 import Indexing from './component/indexing';
 import Viewers from './component/viewers';
 import Analitic from './component/analitics';
+import FACEBOOK_PIXEL_1 from './pixel-1';
 
 export const metadata = {
   title: 'RumahJo',
@@ -25,8 +26,11 @@ const RootLayout = function({ children }) {
 
   return (
     <html lang="en">
-      <Indexing />
-      <link rel="icon" href="/rumahjo.png" type='png' sizes="any" />
+      <head>
+        <Indexing />
+        <link rel="icon" href="/rumahjo.png" type='png' sizes="any" />
+        <FACEBOOK_PIXEL_1 />
+      </head>
       <body style={{overflowX:'hidden'}} className="pt-[40px] md:pt-[60px] bg-gray-50">
           <Providers>
             <Header />
